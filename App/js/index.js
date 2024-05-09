ca78cf2d4aff43881cde7d3fdca14e39a743d25a
Ossie1011 <oscargibbs11@gmail.com>
2024-05-09 16:54:40 +0100

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,20 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input5")
+                .setRequired(true)
+                .setLeft("17.523809523809526em")
+                .setTop("17.523809523809526em")
+                .setWidth("18em")
+                .setLabelSize("8em")
+                .setLabelCaption("Password Input")
+                .setType("password")
+                .setValue("pwd")
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
